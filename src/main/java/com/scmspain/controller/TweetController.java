e9d2974e48262fadecb0073cc53b0cc1c92c7a8b
richard <richyde2005@gmail.com>
2018-09-06 13:29:28 +0200

    Technical test for Fotocasa backend developer @rRichardRoca

--- src/main/java/com/scmspain/controller/TweetController.java
+++ src/main/java/com/scmspain/controller/TweetController.java
@@ -2,6 +2,7 @@ package com.scmspain.controller;
 
 import com.scmspain.controller.command.PublishTweetCommand;
 import com.scmspain.entities.Tweet;
+import com.scmspain.entities.TweetDiscarded;
 import com.scmspain.services.TweetService;
 import org.springframework.web.bind.annotation.*;
 
@@ -23,10 +24,22 @@ public class TweetController {
         return this.tweetService.listAllTweets();
     }
 
-    @PostMapping("/tweet")
+//    @PostMapping("/tweet")
+    @PostMapping(value="/tweet",consumes = "application/json", produces = "application/json")
     @ResponseStatus(CREATED)
     public void publishTweet(@RequestBody PublishTweetCommand publishTweetCommand) {
         this.tweetService.publishTweet(publishTweetCommand.getPublisher(), publishTweetCommand.getTweet());
+    }
+    
+    @PostMapping(value="/discarded",consumes = "application/json", produces = "application/json")
+    @ResponseStatus(CREATED)
+    public void discardTweet(@RequestBody PublishTweetCommand publishTweetCommand) {
+        this.tweetService.discardTweet(publishTweetCommand.getTweet());
+    }
+    
+    @GetMapping("/discarded")
+    public List<TweetDiscarded> listAllTweetsDiscarded() {
+        return this.tweetService.listAllTweetsDiscarded();
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
--- src/main/java/com/scmspain/services/TweetService.java
+++ src/main/java/com/scmspain/services/TweetService.java
@@ -1,14 +1,19 @@
 package com.scmspain.services;
 
 import com.scmspain.entities.Tweet;
+import com.scmspain.entities.TweetDiscarded;
+
 import org.springframework.boot.actuate.metrics.writer.Delta;
 import org.springframework.boot.actuate.metrics.writer.MetricWriter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 import javax.transaction.Transactional;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -18,6 +23,7 @@ public class TweetService {
     private MetricWriter metricWriter;
 
     public TweetService(EntityManager entityManager, MetricWriter metricWriter) {
+
         this.entityManager = entityManager;
         this.metricWriter = metricWriter;
     }
@@ -29,9 +35,24 @@ public class TweetService {
       Result - recovered Tweet
     */
     public void publishTweet(String publisher, String text) {
+    	
+    	String link = "";
+    	int indexLinkHttp,indexLinkHttps;
+    	indexLinkHttp = text.indexOf("http://");
+		indexLinkHttps = text.indexOf("https://");
+		indexLinkHttp = indexLinkHttp > 0? indexLinkHttp:indexLinkHttps;
+		
+		boolean spaceempty = text.endsWith(" ");
+		
+		if ( indexLinkHttp > 0 && spaceempty ) {
+			link = text.substring(indexLinkHttp);
+			text = text.substring(0, indexLinkHttp);
+		}
+    	
         if (publisher != null && publisher.length() > 0 && text != null && text.length() > 0 && text.length() < 140) {
             Tweet tweet = new Tweet();
-            tweet.setTweet(text);
+                      
+           	tweet.setTweet(text.concat(link));
             tweet.setPublisher(publisher);
 
             this.metricWriter.increment(new Delta<Number>("published-tweets", 1));
@@ -49,6 +70,27 @@ public class TweetService {
     public Tweet getTweet(Long id) {
       return this.entityManager.find(Tweet.class, id);
     }
+    
+    public TweetDiscarded getTweetDiscarded(Long id) {
+      return this.entityManager.find(TweetDiscarded.class, id);
+    }
+    
+    public void discardTweet(String idTweet) {
+  	
+    	Tweet tweet= getTweet(Long.valueOf(idTweet).longValue());
+    	
+    	this.entityManager.remove(tweet);
+    	
+    	TweetDiscarded tweetDiscarded = new TweetDiscarded();
+    	tweetDiscarded.setId(tweet.getId());
+    	tweetDiscarded.setPublisher(tweet.getPublisher());
+    	tweetDiscarded.setTweet(tweet.getTweet());
+    	tweetDiscarded.setDate(new Date());
+    	
+    	this.metricWriter.increment(new Delta<Number>("discarded-tweets", 1));
+        this.entityManager.persist(tweetDiscarded);
+    }
+    
 
     /**
       Recover tweet from repository
@@ -62,6 +104,17 @@ public class TweetService {
         List<Long> ids = query.getResultList();
         for (Long id : ids) {
             result.add(getTweet(id));
+        }
+        return result;
+    }
+
+    public List<TweetDiscarded> listAllTweetsDiscarded() {
+        List<TweetDiscarded> result = new ArrayList<TweetDiscarded>();
+        this.metricWriter.increment(new Delta<Number>("times-queried-tweets", 1));
+        TypedQuery<Long> query = this.entityManager.createQuery("SELECT id FROM TweetDiscarded AS tweetId ORDER BY date DES", Long.class);
+        List<Long> ids = query.getResultList();
+        for (Long id : ids) {
+            result.add(getTweetDiscarded(id));
         }
         return result;
     }
